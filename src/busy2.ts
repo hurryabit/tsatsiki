a56e889f398abfc0d0005b6d8de24f33d99da54d
Martin Huschenbett <martin.huschenbett@posteo.me>
2020-10-03 22:58:47 +0200

    Simplify typed version

--- src/busy2.ts
+++ src/busy2.ts
@@ -1,29 +1,23 @@
 import * as Untyped from './busy1';
 
-type TypesSpec = {[name: string]: unknown}
+type TypeSpec = Record<string, unknown>
 
-type Getter<Types extends TypesSpec> =
-    <Name extends keyof Types>(name: Name, key: string) => Types[Name]
-
-type Setter<Types extends TypesSpec> =
-    <Name extends keyof Types>(name: Name, key: string, value: Types[Name]) => void
-
-export type DatabaseReader<Inputs extends TypesSpec, Derivations extends TypesSpec> = {
-    get_value: Getter<Inputs & Derivations>;
+export type DatabaseReader<Inputs extends TypeSpec, Derivations extends TypeSpec> = {
+    get_value<Layer extends keyof (Inputs & Derivations)>(name: Layer, key: string): (Inputs & Derivations)[Layer];
 }
 
-export type Database<Inputs extends TypesSpec, Derivations extends TypesSpec> = DatabaseReader<Inputs, Derivations> & {
-    set_value: Setter<Inputs>
+export type Database<Inputs extends TypeSpec, Derivations extends TypeSpec> = DatabaseReader<Inputs, Derivations> & {
+    set_value<Layer extends keyof Inputs>(name: Layer, key: string, value: Inputs[Layer]): void;
 }
 
-export type DatabaseSpec<Inputs extends TypesSpec, Derivations extends TypesSpec> = {
+export type DatabaseSpec<Inputs extends TypeSpec, Derivations extends TypeSpec> = {
     [Layer in keyof Inputs | keyof Derivations]:
         Layer extends keyof Derivations
-        ? (db: DatabaseReader<Inputs, Derivations>, key: string) => Derivations[Layer]
+        ? (Layer extends keyof Inputs ? never : (db: DatabaseReader<Inputs, Derivations>, key: string) => Derivations[Layer])
         : null;
 }
 
-export function Database<Inputs extends TypesSpec, Derivations extends TypesSpec>(
+export function Database<Inputs extends TypeSpec, Derivations extends TypeSpec>(
     spec: DatabaseSpec<Inputs, Derivations>,
 ): Database<Inputs, Derivations> {
     return Untyped.Database(spec as Untyped.DatabaseSpec) as Database<Inputs, Derivations>;
