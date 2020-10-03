dd0b8de498aa8300e99996bca4adf85b3f013b46
Martin Huschenbett <martin.huschenbett@posteo.me>
2020-10-03 11:22:04 +0200

    Improve naming

--- src/busy1.ts
+++ src/busy1.ts
@@ -21,19 +21,19 @@ type DatabaseReader = {
 
 export type Rule = (reader: DatabaseReader, key: string) => unknown
 
-function with_trace(reader: DatabaseReader): [DatabaseReader, Query[]] {
-    const trace: Query[] = [];
-    const get_value = (name: string, key: string) => {
-        trace.push({ name, key });
-        return reader.get_value(name, key);
-    }
-    return [{get_value}, trace];
-}
-
 export class Database implements DatabaseReader {
     rules: { [name: string]: Rule | null } = {};
     cells: { [name: string]: { [key: string]: Cell } } = {};
-    currently_at: Revision = 0;
+    current_revision: Revision = 0;
+
+    private get_traced_reader(): {reader: DatabaseReader, trace: Query[]} {
+        const trace: Query[] = [];
+        const get_value = (name: string, key: string) => {
+            trace.push({ name, key });
+            return this.get_value(name, key);
+        }
+        return {reader: {get_value}, trace};
+    }
 
     private eval_cell({name, key}: Query): Cell {
         const rule = this.rules[name];
@@ -46,42 +46,42 @@ export class Database implements DatabaseReader {
                 throw Error(`Getting value for unset input ${name}/${key}.`);
             } else {
                 console.log(`Accessing input ${name}/${key}.`)
-                cell.verified_at = this.currently_at;
+                cell.verified_at = this.current_revision;
                 return cell;
             }
         } else if (cell === undefined) {
             console.log(`Evaluating ${name}/${key} for the first time...`);
-            const [reader, trace] = with_trace(this.get_reader());
+            const {reader, trace} = this.get_traced_reader();
             const value = rule(reader, key);
             const cell = {
                 value: value,
                 dependencies: trace,
-                changed_at: this.currently_at,
-                verified_at: this.currently_at,
+                changed_at: this.current_revision,
+                verified_at: this.current_revision,
             };
             this.cells[name][key] = cell;
             console.log(`Evaluated ${name}/${key}.`);
             return cell;
-        } else if (cell.verified_at === this.currently_at) {
+        } else if (cell.verified_at === this.current_revision) {
             console.log(`Taking ${name}/${key} from cache because is has been verified already.`)
             return cell;
         } else {
             let inputs_changed = false;
             for (const dep_query of cell.dependencies) {
                 const dep_cell = this.eval_cell(dep_query);
-                if (dep_cell.changed_at === this.currently_at) {
+                if (dep_cell.changed_at === this.current_revision) {
                     inputs_changed = true;
                     break;
                 }
             }
             if (inputs_changed) {
                 console.log(`Re-evaluating ${name}/${key}...`);
-                const [reader, trace] = with_trace(this.get_reader());
+                const {reader, trace} = this.get_traced_reader();
                 const value = rule(reader, key);
                 cell.dependencies = trace;
                 if (!deepEqual(value, cell.value, {strict: true})) {
                     cell.value = value;
-                    cell.changed_at = this.currently_at;
+                    cell.changed_at = this.current_revision;
                     console.log(`Evaluated ${name}/${key}.`);
                 } else {
                     console.log(`Taking ${name}/${key} from cache because the value is unchanged.`);
@@ -89,7 +89,7 @@ export class Database implements DatabaseReader {
             } else {
                 console.log(`Taking ${name}/${key} from cache because the inputs are unchanged.`);
             }
-            cell.verified_at = this.currently_at;
+            cell.verified_at = this.current_revision;
             return cell;
         }
     }
@@ -107,21 +107,17 @@ export class Database implements DatabaseReader {
     set_value(name: string, key: string, value: unknown): void {
         const cell = this.cells[name][key];
         if (cell === undefined || !deepEqual(value, cell.value, {strict: true})) {
-            this.currently_at += 1;
+            this.current_revision += 1;
             this.cells[name][key] = {
                 value: value,
                 dependencies: [],
-                changed_at: this.currently_at,
-                verified_at: this.currently_at,
+                changed_at: this.current_revision,
+                verified_at: this.current_revision,
             };
         }
     }
 
     get_value(name: string, key: string): unknown {
         return this.eval_cell({name, key}).value;
-    }
-
-    get_reader(): DatabaseReader {
-        return {get_value: (name, key) => this.get_value(name, key)};
     }
 }
